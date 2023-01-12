2a4b5b638de3c5d8e087fd7d23105073c5b8d0ef
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-01-11 22:29:34 -0300

    feat: finished challenge

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,18 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const userWithGames = await this.repository.findOne({id: user_id}) as User
+    return userWithGames 
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query( "SELECT * FROM users WHERE lower(first_name) = Lower($1) AND lower(last_name) = lower($2)",[first_name,last_name])
   }
 }
